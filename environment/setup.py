362da72ddca4b8b57f9abbc1dd46827cde315a56
SantokiHet <santokihet63@gmail.com>
2025-11-19 15:50:32 +0530

    added streamlit

--- environment/setup.py
+++ environment/setup.py
@@ -1,6 +1,6 @@
 '''
-Author : Karan Chauhan
-Github : Karan-Chauhan19
+Author : Santoki Het
+Github : @SantokiHet
 Organization : L.J University
 '''
 from setuptools import  setup, find_packages
@@ -10,9 +10,9 @@ setup(
     version='1.0',  # version number
     description="This project uses a dataset collected from a bank's marketing campaign. The goal is to develop a machine learning model to predict whether a customer will subscribe to a term deposit or not, based on various customer attributes and past campaign details.",  # description of the project
     packages = find_packages(),  # find all packages
-    author='Karan-Chauhan' ,# author of the package
-    author_email='kc879022@gmail.com', # email of the author
-    url='https://github.com/Karan-Chauhan19/Bank-Marketing-Campaign.git', # url of the project
+    author='Santoki-Het' ,# author of the package
+    author_email='santokihet63@gmail.com', # email of the author
+    url='https://github.com/HetSantoki/Bank-Marketing-predection.git', # url of the project
     install_requires=['pandas', 'numpy', 'sklearn', 'matplotlib','seaborn','keras','tensorflow','imbalanced-learn','torch','joblib'],
     # list of the dependencies required by the package
     classifiers=['Programming Language :: python :: 3.12.3']
